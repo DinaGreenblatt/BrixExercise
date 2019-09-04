9be2205ecce8c5d3b679787030eaeff3e441bf65
DinaGreenblatt <54910909+DinaGreenblatt@users.noreply.github.com>
2019-09-04 21:51:15 +0300

    Add files via upload

---
+++ StringMatcher/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMatcher
+{
+    class Program
+    {
+        const string fileName = "data.txt";
+        static Dictionary<string, string> Data = new Dictionary<string, string>();
+        static Dictionary<string, string> lines = new Dictionary<string, string>();
+        static string line;
+        static string sortedString;
+        static string input;
+
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sortedString = Utils.SortString(line);
+                    if (!lines.ContainsKey(sortedString))
+                    {
+                        lines.Add(sortedString, line);
+                    }
+
+                }
+                reader.Close();
+            }
+            //input = new String();
+            while (true)
+            {
+                while (string.IsNullOrEmpty(input) || input.Length != 5)
+                {
+                    Console.WriteLine("Please enter a 5 character string: ");
+                    input = Console.ReadLine();
+                }
+                input = Utils.SortString(input);
+                if (lines.ContainsKey(input))
+                    Console.WriteLine("First match found in data: {0}.", lines[input]);
+                else
+                    Console.WriteLine("No match found.");
+                input = null;
+            }
+
+        }
+    }
+}
+        
+   
+
---
+++ StringMatcher/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMatcher
+{
+    public static class Utils
+    {
+        public static string SortString(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+    }
+}
